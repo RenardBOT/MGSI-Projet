59a68b63fa350bcd4dc3fcb87c9b5bb4a6339d06
RenardBOT <pevan.ers@gmail.com>
2022-12-30 18:50:47 +0100

    Signature de la fonction lisant un fichier OBJ

--- src/obj.cpp
+++ src/obj.cpp
@@ -1,1 +1,3 @@
 #include "obj.hpp"
+
+void readObjFile(const char *filename, vector<vertex> &vertices, vector<face> &faces, vector<halfedge> &halfedges)
--- src/obj.hpp
+++ src/obj.hpp
@@ -5,4 +5,17 @@
 
 #include "mesh_structs.hpp"
 
+using namespace std;
+
+/**
+ * Fonction qui lit un fichier .obj et qui remplit les vecteurs de sommets, faces et demi-arête passés en paramètre
+ *
+ * @param filename Nom du fichier .obj à lire
+ * @param vertices Vecteur de sommets à remplir
+ * @param faces Vecteur de faces à remplir
+ * @param halfedges Vecteur de demi-arêtes à remplir
+ * @return void
+ */
+void readObjFile(const char *filename, vector<vertex> &vertices, vector<face> &faces, vector<halfedge> &halfedges)
+
 #endif /* OBJ_HPP */
