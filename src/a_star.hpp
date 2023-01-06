2d32979d0ed93286d47af0e0aa9d7350858beb96
RenardBOT <pevan.ers@gmail.com>
2023-01-06 21:06:06 +0100

    Ajout d'un fichier pour l'algo A*

---
+++ src/a_star.hpp
@@ -0,0 +1,14 @@
+#ifndef A_STAR_HPP
+#define A_STAR_HPP
+
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+#include "halfedge.hpp"
+
+/* -------------------------------------------------------------------------- */
+/*                                  Méthodes                                  */
+/* -------------------------------------------------------------------------- */
+
+#endif /* A_STAR_HPP */
